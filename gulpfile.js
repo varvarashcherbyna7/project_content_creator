1e0ad657216f5b691a4f99abaa5ffebb514a5b9b
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-05-22 14:07:15 +0300

    img

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const sync = require('browser-sync').create();
 const imageminGiflossy = require('imagemin-giflossy');
 const deploy = require('gulp-gh-pages');
+const netlify = require('gulp-netlify')
 
 
 function html() {
@@ -38,7 +39,7 @@ function css() {
 }
 
 function img() {
-    return src('src/img/**/*.{gif,png,jpg,svg}')
+    return src('src/img/**/*.{gif, png, jpg, svg}')
         .pipe(cache(imagemin([
                 imageminGiflossy({
                     optimizationLevel: 3,
@@ -46,8 +47,15 @@ function img() {
                     lossy: 2
                 }),
             ]))
-            .pipe(dest('build/img')))
+            .pipe(dest('build/img')));
 }
+
+// function img() {
+//     return src('src/img/**/*.*')
+//         .pipe(imagemin())
+//         .pipe(dest('build/img'))
+// }
+
 
 function clear() {
     return del('build')
@@ -65,7 +73,6 @@ exports.build = series(clear, css, html, img)
 exports.serve = series(clear, css, html, img, serve)
 exports.clear = clear
 
-
 /**
  * Push build to gh-pages
  */
@@ -73,3 +80,11 @@ gulp.task('deploy', function() {
     return gulp.src("./build/**/*")
         .pipe(deploy())
 });
+
+// gulp.task('deploy', async function() {
+//     gulp.src('./build/**/*')
+//         .pipe(netlify({
+//             site_id: '613869e6-36a6-43fa-9835-3fb2350f3117',
+//             access_token: 'MP-xN641aog6yaQFMjFiRB7BpwmiIYtGiaE27uRpEEU'
+//         }))
+// })
